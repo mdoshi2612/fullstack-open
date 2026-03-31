1732f5a7f07be9070b1d22075c474aa303b0efa1
Manav Doshi <doshimanav@gmail.com>
2026-03-31 12:37:32 -0700

    Setting up frontend for part5

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -6,6 +6,7 @@ const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
+const cors = require('cors')
 
 const app = express()
 
@@ -20,6 +21,7 @@ mongoose
 
 app.use(express.static('dist'))
 app.use(express.json())
+app.use(cors())
 app.use(middleware.tokenHandler)
 
 app.use('/api/blogs', middleware.userExtractor, blogsRouter)
