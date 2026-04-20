41951614ef22f113862b0db2d98d23b0ab15a78b
Manav Doshi <doshimanav@gmail.com>
2026-04-19 22:03:09 -0700

    finishing healthapp

--- part9/healthapp/bmi.ts
+++ part9/healthapp/bmi.ts
@@ -1,19 +1,19 @@
 export const calculateBmi = (height: number, weight: number): string => {
-  const heightInMetres: number = height / 100
-  const bmi: number = weight / (heightInMetres * heightInMetres)
-  if (bmi <= 16) return 'Underweight (Severe thinness)'
-  else if (bmi <= 17) return 'Underweight (Moderate thinness)'
-  else if (bmi <= 18.5) return 'Underweight (Mild thinness)'
-  else if (bmi <= 25) return 'Normal range'
-  else if (bmi <= 30) return 'Overweight (Pre-obese)'
-  else if (bmi <= 35) return 'Obese (Class I)'
-  else if (bmi <= 40) return 'Obese (Class II)'
-  else return 'Obese (Class III)'
-}
+  const heightInMetres: number = height / 100;
+  const bmi: number = weight / (heightInMetres * heightInMetres);
+  if (bmi <= 16) return 'Underweight (Severe thinness)';
+  else if (bmi <= 17) return 'Underweight (Moderate thinness)';
+  else if (bmi <= 18.5) return 'Underweight (Mild thinness)';
+  else if (bmi <= 25) return 'Normal range';
+  else if (bmi <= 30) return 'Overweight (Pre-obese)';
+  else if (bmi <= 35) return 'Obese (Class I)';
+  else if (bmi <= 40) return 'Obese (Class II)';
+  else return 'Obese (Class III)';
+};
 
 if (process.argv[1] === import.meta.filename) {
-  const height: number = Number(process.argv[2])
-  const weight: number = Number(process.argv[3])
+  const height: number = Number(process.argv[2]);
+  const weight: number = Number(process.argv[3]);
 
-  console.log(calculateBmi(height, weight))
+  console.log(calculateBmi(height, weight));
 }
