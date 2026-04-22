5a92f00ef3cf13ee14e2b9175670f95b614cb2a8
Manav Doshi <doshimanav@gmail.com>
2026-04-21 23:20:35 -0700

    wrapping up healthapp

--- part9/healthapp/index.ts
+++ part9/healthapp/index.ts
@@ -1,35 +1,35 @@
-import express from 'express';
-import { calculateBmi } from './bmi.ts';
+import express from 'express'
+import { calculateBmi } from './bmi.ts'
 
-const app = express();
-app.use(express.json());
+const app = express()
+app.use(express.json())
 
-const PORT: number = 3000;
+const PORT: number = 3000
 
 app.get('/hello', (_req, res) => {
-  res.send('Hello World');
-});
+  res.send('Hello World')
+})
 
 app.get('/bmi', (req, res) => {
-  const { height, weight } = req.query;
+  const { height, weight } = req.query
 
-  const heightNum = Number(height);
-  const weightNum = Number(weight);
+  const heightNum = Number(height)
+  const weightNum = Number(weight)
 
   if (!height || !weight || isNaN(heightNum) || isNaN(weightNum)) {
     res.status(400).send({
       error: 'malformatted parameters',
-    });
-    return;
+    })
+    return
   }
 
   res.send({
     weight: weightNum,
     height: heightNum,
     bmi: calculateBmi(heightNum, weightNum),
-  });
-});
+  })
+})
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
